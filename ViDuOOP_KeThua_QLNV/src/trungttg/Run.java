a2e415b86cdfd9c7511f0f34028a982c3d6d1c87
GiaTrung48 <trung.ttg.66cntt@ntu.edu.vn>
2026-04-19 14:24:33 +0700

    Nộp Ví Dụ OOP Hàm Tạo Getter

--- ViDuOOP_KeThua_QLNV/src/trungttg/Run.java
+++ ViDuOOP_KeThua_QLNV/src/trungttg/Run.java
@@ -2,4 +2,11 @@ package trungttg;
 
 public class Run {
 
+	public static void main(String[] args) {
+		NhanVien nv1 = new NhanVien("NV001","Phan Lập Công",2500000);
+		System.out.print(nv1);		
+		NhanVienVanPhong nvVanPhong1 = new NhanVienVanPhong("NV002", "Nguyễn Văn A", 2000000, 3, 200000);
+		System.out.print("\n" +nvVanPhong1);
+	}
+
 }
