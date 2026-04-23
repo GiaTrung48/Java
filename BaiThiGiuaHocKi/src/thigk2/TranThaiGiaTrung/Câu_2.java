020033d4c04ad46a264a8bfe64932ed47fe88b96
GiaTrung48 <trung.ttg.66cntt@ntu.edu.vn>
2026-04-23 07:57:35 +0700

    LAN THU 2

---
+++ BaiThiGiuaHocKi/src/thigk2/TranThaiGiaTrung/Câu_2.java
@@ -0,0 +1,13 @@
+package thigk2.TranThaiGiaTrung;
+
+public class Câu_2 {
+	class SinhVien {
+		private String maSV;
+		private String hoTen;
+		private int namSinh;
+		private String chuyenNganh;
+	}
+	public SinhVien(String maSV, String hoTen, int namSinh, String chuyenNganh) {
+		this.maSV = maSV
+	}
+}
---
+++ BaiThiGiuaHocKi/src/thigk2/TranThaiGiaTrung/MainCau2.java
@@ -0,0 +1,10 @@
+package thigk2.TranThaiGiaTrung;
+
+public class MainCau2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
